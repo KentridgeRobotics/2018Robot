fdd5a427f7519600a3c5f2f8ea35740b58f7fa53
ChilingIce <spotbear@yahoo.com>
2018-01-13 14:59:31 -0800

    Added a Mecanum Drive variable.

--- 2018 Competition Robot/src/org/usfirst/frc/team3786/robot/subsystems/WheelsSubsystem.java
+++ 2018 Competition Robot/src/org/usfirst/frc/team3786/robot/subsystems/WheelsSubsystem.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team3786.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 
 
 /**
@@ -17,12 +18,14 @@ public class WheelsSubsystem extends Subsystem {
 	private WPI_TalonSRX rightBack;
 	private WPI_TalonSRX rightFront;
 	
+	private MecanumDrive mecanumDrive;
+	
 	public WheelsSubsystem() {
 		leftFront = new WPI_TalonSRX(0);
 		leftBack = new WPI_TalonSRX(0);
 		rightBack = new WPI_TalonSRX(0);
 		rightFront = new WPI_TalonSRX(0);
-		
+		mecanumDrive = new MecanumDrive(leftFront, leftBack, rightFront, rightBack);
 	}
 
 	public void setMotorSpeeds(double leftFrontSpeed, double leftBackSpeed, double rightBackSpeed, double rightFrontSpeed) {
