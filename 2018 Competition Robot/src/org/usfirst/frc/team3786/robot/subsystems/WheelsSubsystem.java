257cad3a630b98e867a08393ddab1b9d756c8af0
ChilingIce <spotbear@yahoo.com>
2018-01-13 13:36:16 -0800

    Added Wheel subsystem.

---
+++ 2018 Competition Robot/src/org/usfirst/frc/team3786/robot/subsystems/WheelsSubsystem.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team3786.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+
+/**
+ *
+ */
+public class WheelsSubsystem extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	private WPI_TalonSRX leftFront;
+	private WPI_TalonSRX leftBack;
+	private WPI_TalonSRX rightBack;
+	private WPI_TalonSRX rightFront;
+	
+	public WheelsSubsystem() {
+		leftFront = new WPI_TalonSRX(0);
+		leftBack = new WPI_TalonSRX(0);
+		rightBack = new WPI_TalonSRX(0);
+		rightFront = new WPI_TalonSRX(0);
+		
+	}
+
+	public void setMotorSpeeds(double leftFrontSpeed, double leftBackSpeed, double rightBackSpeed, double rightFrontSpeed) {
+		leftFront.set(leftFrontSpeed);
+		leftBack.set(leftBackSpeed);
+		rightBack.set(rightBackSpeed);
+		rightFront.set(rightFrontSpeed);
+		
+	}
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
