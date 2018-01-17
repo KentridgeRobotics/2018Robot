fe016c201fa08d1179e58758e3a0f084cf495548
sn328541 <sn328541@KRI2020-31945M.kent.k12.wa.us>
2018-01-17 14:02:32 -0800

    Made some changes in two wheel subsystem.

--- 2018 Competition Robot/src/org/usfirst/frc/team3786/robot/subsystems/TwoWheelSubsytem.java
+++ 2018 Competition Robot/src/org/usfirst/frc/team3786/robot/subsystems/TwoWheelSubsytem.java
@@ -1,5 +1,43 @@
 package org.usfirst.frc.team3786.robot.subsystems;
 
-public class TwoWheelSubsytem {
+public class TwoWheelSubsytem implements Drive {
+	private WPI_TalonSRX left;
+	private WPI_TalonSRX right;
+	public TwoWheelSubsystem() {
+		left = new WPI_TalonSRX(1);
+		right = new WPI_TalonSRX(2);
+		
+	}
+	@Override
+	public void setMotorSpeeds(double leftFrontSpeed, double leftBackSpeed, double rightBackSpeed, double rightFrontSpeed) {
+		left.set(leftFrontSpeed);
+		right.set(rightFrontSpeed);
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	@Override
+	public void setBrakeMode(boolean isBraking) {
+		NeutralMode mode;
+		if(isBraking) {
+			mode = NeutralMode.Brake;
+		} else {
+			mode = NeutralMode.Coast;
+		}
+		left.setNeutralMode(mode);
+		right.setNeutralMode(mode);
+		
+		
+		
+	}
+
+	@Override
+	public void setTwoMotorSpeeds(double leftSpeed, double rightSpeed) {
+		left.set(leftSpeed);
+		right.set(rightSpeed);
+		// TODO Auto-generated method stub
+		
+	}
 
 }
