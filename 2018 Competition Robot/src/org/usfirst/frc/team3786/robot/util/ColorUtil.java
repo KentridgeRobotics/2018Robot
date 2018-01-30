deef2fc4890e8d9368e7adf90b79b04e0ec00dff
SN329554 <SN329554@KRI2020-32531M.kent.k12.wa.us>
2018-01-29 18:13:48 -0800

    Added template Color Sensor Util

---
+++ 2018 Competition Robot/src/org/usfirst/frc/team3786/robot/util/ColorUtil.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team3786.robot.util;
+
+public class ColorUtil {
+	
+	public ColorUtil() {
+		//TODO Init Color Sensor
+	}
+	
+	public boolean onBlack() {
+		//TODO Check if on black tape
+		return false;
+	}
+
+}
