34aa62c78997da4edba409ecebe85e8f8865a153
Chris DuPuis <cpdupuis@gmail.com>
2018-02-03 14:04:33 -0800

    Added lifter subsystem, lifter raise command, and lifter lower command.

--- 2018 Competition Robot/src/org/usfirst/frc/team3786/robot/commands/LifterRaiseCommand.java
+++ 2018 Competition Robot/src/org/usfirst/frc/team3786/robot/commands/LifterRaiseCommand.java
@@ -1,5 +1,44 @@
 package org.usfirst.frc.team3786.robot.commands;
 
-public class LifterRaiseCommand {
+import org.usfirst.frc.team3786.robot.subsystems.LifterSubsystem;
 
-}
+import edu.wpi.first.wpilibj.command.Command;
+
+public class LifterRaiseCommand extends Command{
+	
+	public LifterRaiseCommand() {
+		// Use requires() here to declare subsystem dependencies
+		requires(LifterSubsystem.getInstance());
+	}
+	
+	// Called just before this Command runs the first time
+		@Override
+		protected void initialize() {
+			LifterSubsystem.getInstance().setBrakeMode(false);
+			LifterSubsystem.getInstance().setLiftSpeed(1.0);
+		}
+
+		// Called repeatedly when this Command is scheduled to run
+		@Override
+		protected void execute() {
+		}
+
+		// Make this return true when this Command no longer needs to run execute()
+		@Override
+		protected boolean isFinished() {
+			return false;
+		}
+
+		// Called once after isFinished returns true
+		@Override
+		protected void end() {
+		}
+
+		// Called when another command which requires one or more of the same
+		// subsystems is scheduled to run
+		@Override
+		protected void interrupted() {
+		}
+	}
+		
+
--- 2018 Competition Robot/src/org/usfirst/frc/team3786/robot/subsystems/LifterSubsystem.java
+++ 2018 Competition Robot/src/org/usfirst/frc/team3786/robot/subsystems/LifterSubsystem.java
@@ -1,5 +1,52 @@
 package org.usfirst.frc.team3786.robot.subsystems;
 
-public class LifterSubsystem {
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class LifterSubsystem extends Subsystem {
+	
+private static LifterSubsystem instance;
+	
+	public static LifterSubsystem getInstance() {
+		if(instance == null)
+			instance = new LifterSubsystem();
+		return instance;
+	}
+	
+	private WPI_TalonSRX Lifter;
+	private WPI_TalonSRX Winch;
+	
+	
+	public LifterSubsystem() {
+		Lifter = new WPI_TalonSRX(7);
+		Winch = new WPI_TalonSRX(8);
+	}
+
+	public void setBrakeMode(boolean isBraking) {
+		NeutralMode mode;
+		if(isBraking) {
+			mode = NeutralMode.Brake;
+		} else {
+			mode = NeutralMode.Coast;
+		}
+		Lifter.setNeutralMode(mode);
+
+		
+		
+		
+	}
+	
+	public void setLiftSpeed(double speed) {
+		Lifter.set(speed);
+	}
+	
+	@Override
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+	
+	
 
 }
