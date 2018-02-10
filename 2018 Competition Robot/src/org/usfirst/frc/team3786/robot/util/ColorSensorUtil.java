65bf5fda066a60fd69e539866ddd5d621b7099ed
krrobotics <krrobotics@mithrandir>
2018-02-09 19:05:39 -0800

    added some methods in ColorSensorUtil

--- 2018 Competition Robot/src/org/usfirst/frc/team3786/robot/util/ColorSensorUtil.java
+++ 2018 Competition Robot/src/org/usfirst/frc/team3786/robot/util/ColorSensorUtil.java
@@ -1,14 +1,43 @@
 package org.usfirst.frc.team3786.robot.util;
 
-public class ColorSensorUtil {
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
+public class ColorSensorUtil {
+	
+	public double getCurrentHue() {
+		NetworkTableInstance ntInst = NetworkTableInstance.getDefault();
+		NetworkTable visionTable = ntInst.getTable("vision");
+		double value = visionTable.getEntry("Hue").getDouble(0.0);
+		return value;
+		
+	}
+	
+	public double getCurrentSaturation() {
+		NetworkTableInstance ntInst = NetworkTableInstance.getDefault();
+		NetworkTable visionTable = ntInst.getTable("vision");
+		double value = visionTable.getEntry("Saturation").getDouble(0.0);
+		return value;
+
+	}
+	
+	public double getCurrentLuminosity() {
+		NetworkTableInstance ntInst = NetworkTableInstance.getDefault();
+		NetworkTable visionTable = ntInst.getTable("vision");
+		double value = visionTable.getEntry("Luminosity").getDouble(0.0);
+		return value;
+	}
+	
 	public ColorSensorUtil() {
+		
 		// TODO Color sensor init
 	}
 
 	public boolean onBlackTape() {
+		
 		// TODO Return whether or not on black tape
 		return true;
+		
 	}
 
 }
