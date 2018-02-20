964e936b13ddf45e4fe53af85006b3f32eb4dff5
krrobotics <krrobotics@3786-G580.kent.k12.wa.us>
2018-02-20 13:38:53 -0800

    Updated TwoWheelSubsystem

--- 2018 Competition Robot/src/org/usfirst/frc/team3786/robot/subsystems/TwoWheelSubsystem.java
+++ 2018 Competition Robot/src/org/usfirst/frc/team3786/robot/subsystems/TwoWheelSubsystem.java
@@ -9,18 +9,23 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class TwoWheelSubsystem extends Subsystem implements ChargersDriveSubsystem {
 
 	private WPI_TalonSRX left;
 	private WPI_TalonSRX right;
 
+	private DifferentialDrive differentialDrive;
+	
 	public TwoWheelSubsystem() {
 		left = new WPI_TalonSRX(RobotMap.leftMotor);
 		right = new WPI_TalonSRX(RobotMap.rightMotor);
 
-		left.configOpenloopRamp(0.3, 0);
-		right.configOpenloopRamp(0.3, 0);
+//		left.configOpenloopRamp(0.3, 0);
+//		right.configOpenloopRamp(0.3, 0);
+		
+		differentialDrive = new DifferentialDrive(left, right);
 	}
 
 	public void setMotorSpeeds(double leftSpeed, double rightSpeed) {
@@ -45,7 +50,8 @@ public class TwoWheelSubsystem extends Subsystem implements ChargersDriveSubsyst
 
 	@Override
 	public void gyroAssistedDrive(double x, double y, double turnRate) {
-		// TODO Auto-generated method stub
+		
+		differentialDrive.arcadeDrive(y, turnRate);
 		
 	}
 
